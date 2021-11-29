129b0a4c5bfeda8d7419767fb851ea2292698dd9
Unisse Chua <20331584+unissechua@users.noreply.github.com>
2021-11-29 11:18:56 +0800

    Initial commit

---
+++ controllers/post.controller.js
@@ -0,0 +1,15 @@
+const PostModel = require('../models/post.model');
+const PostController = {};
+
+PostController.create = (req, res) => {
+    return PostModel.createPost(req.body, (err, post) => {
+        if (err) {
+            return res.status(500).end();
+        } else {
+            return res.json(post);
+        }
+    })
+
+};
+
+module.exports = PostController;
